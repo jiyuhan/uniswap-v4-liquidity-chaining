c4a3c1717c95b69268dce99b22fd0091058b1d92
Thomas Han <thomashanthomashan@gmail.com>
2023-09-24 02:56:13 -0400

    adding sc

--- src/LChain.sol
+++ src/LChain.sol
@@ -1,1 +1,9 @@
-// contract LChain 
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {BaseHook} from "v4-periphery/BaseHook.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+
+contract LChain is BaseHook, ERC1155 {
+
+}
